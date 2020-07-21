22331c9fb0de1cf65489125e783a2ea0ac5c5bf3
Calum Henderson <calumproh28@gmail.com>
2020-07-21 13:44:29 +0100

    Added refs and other stuff.

--- swinc.multiplayer/Core/Meta.cs
+++ swinc.multiplayer/Core/Meta.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
-namespace Core
+namespace Multiplayer.Core
 {
-    public class Meta
+    public class Meta : ModMeta
     {
+        public static bool GiveMeFreedom = true;
+        public override string Name => "Software Inc Multiplayer";
+        public override void ConstructOptionsScreen(RectTransform parent, bool inGame)
+        {
+            
+        }
+        public override void Initialize(ModController.DLLMod parentMod)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (x, y) => Assembly.LoadFrom(Path.Combine(parentMod.FolderPath(), "References\\" + y.Name.Substring(0, y.Name.IndexOf(",")) + ".dll"));
+            base.Initialize(parentMod);
+        }
     }
 }
--- swinc.multiplayer/Core/Properties/AssemblyInfo.cs
+++ swinc.multiplayer/Core/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Core")]
+[assembly: AssemblyTitle("Software Inc Multiplayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Core")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright daRedLoCo and CyaCal © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
