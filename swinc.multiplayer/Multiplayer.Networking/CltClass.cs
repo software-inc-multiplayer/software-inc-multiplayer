dc08a2dc22d2bd13604266f3714137e1e760ba04
Roman Wanner <roman_wanner@rowa-digital.ch>
2020-07-22 08:05:15 -0300

    Added Multiplayer.Networking

---
+++ swinc.multiplayer/Multiplayer.Networking/CltClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyTcp3;
+using EasyTcp3.ClientUtils;
+using EasyTcp3.EasyTcpPacketUtils;
+using EasyTcp3.Server.ServerUtils;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Steamworks;
+
+namespace Multiplayer.Networking
+{
+	public class CltClass
+	{
+        EasyTcpClient client;
+        bool isLoggedin = false;
+
+        void Log(string message)
+		{
+            DevConsole.Console.Log("Client: " + message);
+		}
+
+        public void Connect(string server, ushort port, string password = "", Helpers.UserRole userrole = Helpers.UserRole.Client)
+        {
+            client = new EasyTcpClient()
+            {
+                Serialize = o => Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(o)),
+                Deserialize = (bytes, type) => JsonConvert.DeserializeObject(Encoding.UTF8.GetString(bytes), type)
+            };
+            client.OnConnect += (sender, client) => Log("Client connected!");
+            client.OnDisconnect += (sender, client) => Log("Client disconnected!");
+            client.OnDataReceive += OnDataReceive;
+            client.OnDataSend += OnDataSend;
+
+            string un = $"Player";
+            if (!client.Connect(server, port)) return;
+			try
+			{
+                un = SteamFriends.GetPersonaName();
+            }catch(Exception ex)
+			{
+                Log("Couldn't load Steam Username: " + ex.Message);               
+            }
+
+            //Login User
+            Log("Log into server...");
+            Helpers.SystemMessage loginmsg = new Helpers.SystemMessage(Helpers.SysMessageType.Login,un,password,userrole);
+            client.Send(loginmsg.AsMessage(),true);
+        }
+
+        private void OnDataReceive(object sender, Message e)
+        {
+            Helpers.MessageData data = e.Decompress().Deserialize<Helpers.MessageData>();
+            JObject jo = (JObject)data.Data;
+            if (data.DataType == typeof(Helpers.SystemMessage).FullName)
+            {
+                HandleSysMessage(jo.ToObject<Helpers.SystemMessage>(), e.Client);
+            }
+            else if (data.DataType == typeof(Helpers.ChatMessage).FullName)
+            {
+               // HandleChatMessage(jo.ToObject<Helpers.ChatMessage>());
+            }
+        }
+
+        void HandleSysMessage(Helpers.SystemMessage message, EasyTcpClient client)
+        {
+            if (message.MessageType == Helpers.SysMessageType.Login)
+            {
+                string response = (string)message.Data[0];
+                if (response != "ok")
+				{
+                    Log("Client couldn't login! => " + response);
+                    client.Dispose();
+                    return;
+                }
+                Log("Client logged in!");
+            }
+        }
+
+        private void OnDataSend(object sender, Message message)
+        {
+
+        }
+    }
+}
---
+++ swinc.multiplayer/Multiplayer.Networking/Helpers.cs
@@ -0,0 +1,82 @@
+﻿using EasyTcp3;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Multiplayer.Networking
+{
+	public static class Helpers
+	{
+		public class User
+		{
+			public string Username { get; set; }
+			public UserRole Role { get; set; }
+
+			public string ToJson()
+			{
+				return JsonConvert.SerializeObject(this);
+			}
+
+			public static User FromJson(string json)
+			{
+				return JsonConvert.DeserializeObject<User>(json);
+			}
+		}
+
+		public class ChatMessage
+		{
+			public string Sender { get; set; }
+			public string Receiver { get; set; }
+			public string Chatmessage { get; set; }
+			public MessageData AsMessage()
+			{
+				return MessageData.FromObject(this);
+			}
+		}
+
+		public class SystemMessage
+		{
+			public SysMessageType MessageType { get; set; }
+			public List<object> Data { get; set; }
+
+			public SystemMessage(SysMessageType messageType, params object[] data)
+			{
+				MessageType = messageType;
+				Data = new List<object>();
+				Data.AddRange(data);
+			}
+
+			public MessageData AsMessage()
+			{
+				return MessageData.FromObject(this);
+			}
+		}
+
+		public class MessageData
+		{
+			public string DataType { get; set; }
+			public object Data { get; set; }
+
+			public static MessageData FromObject<T>(T obj)
+			{
+				return new MessageData() { Data = obj, DataType = typeof(T).FullName };
+			}
+		}
+
+		public enum SysMessageType
+		{
+			Login
+		}
+
+		public enum UserRole
+		{
+			Host,
+			Client
+		}
+	}
+}
