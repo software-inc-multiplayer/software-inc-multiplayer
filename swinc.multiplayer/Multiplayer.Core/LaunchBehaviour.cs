1bdae1bf76375d975023d23e5f1deb1d263fd252
Calum Henderson <calumproh28@gmail.com>
2020-08-10 22:57:41 +0100

    Added MainMenu button, Localization.
    Created better settings handler.

--- swinc.multiplayer/Multiplayer.Core/LaunchBehaviour.cs
+++ swinc.multiplayer/Multiplayer.Core/LaunchBehaviour.cs
@@ -5,11 +5,14 @@ using System.Text;
 using Multiplayer.Networking;
 using Multiplayer.Debugging;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Multiplayer.Core
 {
     public class LaunchBehaviour : ModBehaviour
     {
+        public static List<GameObject> ActiveObjects = new List<GameObject>() { };
+
         public override void OnActivate()
         {
             new ServerClass().Start();
@@ -17,6 +20,11 @@ namespace Multiplayer.Core
 
         public override void OnDeactivate()
         {
+            foreach(GameObject e in ActiveObjects)
+            {
+                e.SetActive(false);
+                ActiveObjects.Remove(e);
+            }
             ServerClass.Instance.Stop();
         }
     }
---
+++ swinc.multiplayer/Multiplayer.Core/MainMenuManager.cs
@@ -0,0 +1,48 @@
+﻿using Multiplayer.Debugging;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Multiplayer.Core
+{
+    public class MainMenuManager : ModBehaviour
+    {
+        public override void OnActivate()
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            if (currentScene.name == "MainMenu")
+            {
+                CreateButton();
+            }
+            SceneManager.sceneLoaded += SceneChanged;
+        }
+        private void CreateButton()
+        {
+            RectTransform mainMenuPanel = WindowManager.FindElementPath("SPanel / Panel");
+            Button mainMenuButton = WindowManager.SpawnButton();
+            mainMenuButton.GetComponentInChildren<Text>().text = "Multiplayer";
+            mainMenuButton.GetComponentInChildren<Text>().fontSize = 40;
+            mainMenuButton.onClick.AddListener(MainMenuButtonClick);
+            mainMenuButton.gameObject.name = "MainMenu_MP_Button";
+            LaunchBehaviour.ActiveObjects.Add(mainMenuButton.gameObject);
+            WindowManager.AddElementToElement(mainMenuButton.gameObject, mainMenuPanel.gameObject, new Rect(0, 0, 400, 250), Rect.zero);
+
+            Logging.Debug("Added menu button.");
+        }
+        private void SceneChanged(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name != "MainMenu") return;
+            CreateButton();
+        }
+
+        private void MainMenuButtonClick()
+        {
+            WindowManager.SpawnDialog("CS_Dialog".LocDef("Coming soon."), false, DialogWindow.DialogType.Information);
+        }
+
+        public override void OnDeactivate()
+        {
+
+        }
+    }
+}
