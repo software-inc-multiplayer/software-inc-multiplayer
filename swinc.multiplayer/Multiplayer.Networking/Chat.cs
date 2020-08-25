aae8e9a5dbd2a000111f1bea89553fb6dfea6af9
CyaCal <calumproh28@gmail.com>
2020-08-25 18:19:32 +0100

    Fix chat

--- swinc.multiplayer/Multiplayer.Networking/Chat.cs
+++ swinc.multiplayer/Multiplayer.Networking/Chat.cs
@@ -35,7 +35,9 @@ namespace Multiplayer.Networking
                     author = new Helpers.User() { Username = "Server" };
                 ChatMessage msg = new ChatMessage((string)raw.Data.GetValue("message"), author);
                 Logging.Info($"[Chat] {msg.Author.Username}: ${msg.Message}");
+                Logging.Info(instance.Messages.Count);
                 instance.Messages.Add(msg.ID, msg);
+                Logging.Info(instance.Messages.Count);
             } catch(Exception e)
             {
                 Logging.Warn("[Chat] Error parsing chat: " + e);
@@ -47,34 +49,40 @@ namespace Multiplayer.Networking
         /// </summary>
         public static void ClearHistory(bool forced = false)
         {
-            if(forced)
+            try
             {
-                if (instance.Messages.Count == 0)
+                if (forced)
                 {
-                    Logging.Warn($"[Chat] No messages to clear.");
+                    if (instance.Messages.Count == 0)
+                    {
+                        Logging.Warn($"[Chat] No messages to clear.");
+                        return;
+                    }
+                    Logging.Warn($"[Chat] Clearing {instance.Messages.Count} chat messages.");
+                    instance.Messages.Clear();
                     return;
                 }
-                Logging.Warn($"[Chat] Clearing {instance.Messages.Count} chat messages.");
+                if (instance.Messages.Count == 0)
+                {
+                    Logging.Warn($"[Chat] No messages needed to clear/log.");
+                    return;
+                }
+                string logFolderPath = Path.Combine(ModController.ModFolder, "Multiplayer", "Logs");
+                if (!Directory.Exists(logFolderPath)) Directory.CreateDirectory(logFolderPath);
+                string arr = "";
+                foreach (KeyValuePair<ulong, ChatMessage> keyValuePair in instance.Messages)
+                {
+                    ChatMessage msg = keyValuePair.Value;
+                    arr += $"{msg.SentDate.ToShortDateString()} - {msg.Author.Username} said {msg.Message}\n";
+                }
+                File.WriteAllText(Path.Combine(logFolderPath, $"chat-{DateTime.Now.ToShortDateString()}.log"), arr);
+                Logging.Info($"[Chat] Wrote chat logs to {Path.Combine(logFolderPath, $"chat-{DateTime.Now.ToShortDateString()}.log")}");
+                Logging.Info($"[Chat] Cleared Chat History.");
                 instance.Messages.Clear();
-                return;
-            }
-            if(instance.Messages.Count == 0)
+            } catch (Exception e)
             {
-                Logging.Warn($"[Chat] No messages needed to clear/log.");
-                return;
-            }
-            string logFolderPath = Path.Combine(ModController.ModFolder, "Multiplayer", "Logs");
-            if (!Directory.Exists(logFolderPath)) Directory.CreateDirectory(logFolderPath);
-            string arr = "";
-            foreach(KeyValuePair<ulong, ChatMessage> keyValuePair in instance.Messages)
-            {
-                ChatMessage msg = keyValuePair.Value;
-                arr += $"{msg.SentDate.ToShortDateString()} - {msg.Author.Username} said {msg.Message}\n";
-            }
-            File.WriteAllText(Path.Combine(logFolderPath, $"chat-{DateTime.Now.ToShortDateString()}.log"), arr);
-            Logging.Info($"[Chat] Wrote chat logs to {Path.Combine(logFolderPath, $"chat-{DateTime.Now.ToShortDateString()}.log")}");
-            Logging.Info($"[Chat] Cleared Chat History.");
-            instance.Messages.Clear();
+                Logging.Error("There was an error clearing chat:" + e.ToString());
+            }            
         }
     }
 }
