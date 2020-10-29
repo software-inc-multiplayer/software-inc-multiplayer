43fc89d279f409fd7df997fa6517f4b0d85381fb
daredloco <deadlybeatz@hotmail.com>
2020-10-29 08:15:13 -0300

    Added and positioned host and connect buttons
    - The buttons to host and connect to a server are inside options->mods->multiplayer
    
    - Removed errortest

--- swinc.multiplayer/Multiplayer.Core/Meta.cs
+++ swinc.multiplayer/Multiplayer.Core/Meta.cs
@@ -16,16 +16,29 @@ namespace Multiplayer.Core
         public override string Name => "Software Inc Multiplayer";
         public override void ConstructOptionsScreen(RectTransform parent, bool inGame)
         {
-            Button e = WindowManager.SpawnButton();
-            e.onClick.AddListener(ErrorTest);
-            e.SetText("Test Error");
-            WindowManager.AddElementToElement(e.gameObject, parent.gameObject, new Rect(0, 15, 192, 64), Rect.zero);
+            Button bthost = WindowManager.SpawnButton();
+            bthost.onClick.AddListener(ShowMultiplayerHost);
+            bthost.SetText("Host Server");
+            WindowManager.AddElementToElement(bthost.gameObject, parent.gameObject, new Rect(15, 15, 192, 64), Rect.zero);
+            
+            Button btconnect = WindowManager.SpawnButton();
+            btconnect.onClick.AddListener(ShowMultiplayerConnect);
+            btconnect.SetText("Connect to Server");
+            WindowManager.AddElementToElement(btconnect.gameObject, parent.gameObject, new Rect(15, 100, 192, 64), Rect.zero);
+
+
+
         }
 
-        private void ErrorTest()
-        {
-            Logging.Error("Test");
-        }
+		private void ShowMultiplayerConnect()
+		{
+
+		}
+
+		private void ShowMultiplayerHost()
+		{
+
+		}
 
         public override void Initialize(ModController.DLLMod parentMod)
         {
