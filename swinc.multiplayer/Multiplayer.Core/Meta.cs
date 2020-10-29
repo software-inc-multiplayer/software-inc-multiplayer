0cb959568adbab883dd85c3471dba1d3f0cd01a2
daredloco <deadlybeatz@hotmail.com>
2020-10-29 08:30:47 -0300

    Changed two button multiplayer solution to one button

--- swinc.multiplayer/Multiplayer.Core/Meta.cs
+++ swinc.multiplayer/Multiplayer.Core/Meta.cs
@@ -1,6 +1,8 @@
 ﻿using Multiplayer.Debugging;
 using Multiplayer.Extensions;
+using Multiplayer.Networking;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -13,32 +15,33 @@ namespace Multiplayer.Core
     {
         public static ModController.DLLMod ThisMod { get; set; }
         public static bool GiveMeFreedom = true;
+        public GUIWindow MPWindow { get; set; }
         public override string Name => "Software Inc Multiplayer";
         public override void ConstructOptionsScreen(RectTransform parent, bool inGame)
         {
             Button bthost = WindowManager.SpawnButton();
-            bthost.onClick.AddListener(ShowMultiplayerHost);
-            bthost.SetText("Host Server");
+            bthost.onClick.AddListener(CreateBaseMultiplayerWindow);
+            bthost.SetText("Start");
             WindowManager.AddElementToElement(bthost.gameObject, parent.gameObject, new Rect(15, 15, 192, 64), Rect.zero);
-            
-            Button btconnect = WindowManager.SpawnButton();
-            btconnect.onClick.AddListener(ShowMultiplayerConnect);
-            btconnect.SetText("Connect to Server");
-            WindowManager.AddElementToElement(btconnect.gameObject, parent.gameObject, new Rect(15, 100, 192, 64), Rect.zero);
-
-
-
         }
 
-		private void ShowMultiplayerConnect()
-		{
+        private void CreateBaseMultiplayerWindow()
+        {
+            Logging.Info("Opened multiplayer window.");
+            if (MPWindow != null)
+            {
+                MPWindow.Show();
+                return;
+            }
+            MPWindow = WindowManager.SpawnWindow();
+            MPWindow.SetTitle("MultiplayerButton".LocDef("Multiplayer"));
+            MPWindow.ShowCentered = true;
+            MPWindow.MinSize = new Vector2(730, 500);
+            MPWindow.SizeButton.SetActive(false);
 
-		}
 
-		private void ShowMultiplayerHost()
-		{
-
-		}
+            MPWindow.Show();
+        }
 
         public override void Initialize(ModController.DLLMod parentMod)
         {
